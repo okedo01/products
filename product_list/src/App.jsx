523a6f2eded33bd9722f4efca026704eb059261b
Okedo01 <eliahmwelangi01@gmail.com>
2026-07-21 18:00:14 +0300

    Getting started

--- product_list/src/App.jsx
+++ product_list/src/App.jsx
@@ -1,5 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import FoodList from './components/FoodList'
+import FoodItems from './components/FoodItems'
+
 export default function App() {
-  return <div>
-    <h1>Product List</h1>
-  </div>
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("/data.json")
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(err => console.log(err))
+  }, [])
+  return (
+    <div>
+      <h1>Food Products</h1>
+      { products.map(product=> (
+        <div key={product.name}>
+          <h2>{product.name}</h2>
+          <h3>{product.category}</h3>
+        </div>
+      ))}
+    </div>
+  )
 }
---
+++ product_list/src/components/FoodList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FoodList() {
+  return (
+    <div>
+        <h1>Food List</h1>
+    </div>
+  )
+}
