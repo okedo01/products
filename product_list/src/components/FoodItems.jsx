1aba070ca66e19ff7291cdda9a141292f0d04bed
Okedo01 <eliahmwelangi01@gmail.com>
2026-07-22 16:45:22 +0300

    Installing Tailwind css for the projct

--- product_list/src/components/FoodItems.jsx
+++ product_list/src/components/FoodItems.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function FoodItems({product}) {
   return (
     <div>
-      <h2>{product.name}</h2>
+      <h2 className='bg-red-400'>{product.name}</h2>
           <h3>{product.category}</h3>
           <p>${product.price}</p>
           <img src={product.image.thumbnail} />
