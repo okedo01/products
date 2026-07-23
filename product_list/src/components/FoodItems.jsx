fd280bd0b36de796de63228ceca55414029988a3
Okedo01 <eliahmwelangi01@gmail.com>
2026-07-23 11:44:18 +0300

    Image Icon

--- product_list/src/components/FoodItems.jsx
+++ product_list/src/components/FoodItems.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import imageIcon from "../design/icon-add-to-cart.svg"
 
 export default function FoodItems({product}) {
   return (
     <div className=''>
-      <img className='w-full rounded-2xl object-cover' src={product.image.desktop} />
+      <div className="">
+        <img className='w-full rounded-2xl object-cover' src={product.image.desktop} />
+        <div className="bg-amber-300 flex justify-center items-center gap-5 text-xs font-bold w-36 px-4 py-1.5 rounded-2xl">
+          <img src={imageIcon} alt="" />
+          <span className=''>Add to Cart</span>
+        </div>
+      </div>
           <span>{product.category}</span>
           <h3>{product.name}</h3>
           <p>${product.price}</p>
