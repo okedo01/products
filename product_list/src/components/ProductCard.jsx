8d5bb6eac2db21dd4e982d929a8c40aa68ebfbef
Okedo01 <eliahmwelangi01@gmail.com>
2026-08-06 15:44:10 +0300

    Styling the UI component

--- product_list/src/components/ProductCard.jsx
+++ product_list/src/components/ProductCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import cartIcon from "../design/icon-add-to-cart.svg"
 
-export default function ProductCard({product}) {
-  return (
-    <div className='product-card relative'>
-        <img src={product.image.desktop} alt={product.name} />
-        <p>{product.category}</p>
-        <h2>{product.name}</h2>
-        <p>${product.price.toFixed(2)}</p>
-        <button className='absolute bottom-20 left-16 text-sm text-gray-200 bold bg-red-500'>Add to Cart</button>
-    </div>
-  )
+export default function ProductCard({ product }) {
+    return (
+        <div className='product-card relative'>
+            <img src={product.image.desktop} alt={product.name} />
+            <p>{product.category}</p>
+            <h2>{product.name}</h2>
+            <p>${product.price.toFixed(2)}</p>
+            <button className="absolute bottom-20 left-1/2 -translate-x-1/2 flex items-center gap-1 bg-white border rounded-full px-5 py-2">
+                <img src={cartIcon} alt="Cart" className="w-4 h-4" />
+                <span className='text-xs'>Add to Cart</span>
+            </button>
+        </div>
+    )
 }
